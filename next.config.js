5b65c77e98172d7ec2d4269ca2710a27e28bfd39
Felipe Brito <89111098+feripe1@users.noreply.github.com>
2022-10-24 17:15:58 -0300

    next config bug

--- next.config.js
+++ next.config.js
@@ -4,14 +4,6 @@ const nextConfig = {
   swcMinify: true,
   images: {
     domains: ['raw.githubusercontent.com', 'avatars.githubusercontent.com'],
-    remotePatterns: [
-      {
-        protocol: 'https',
-        hostname: 'assets.vercel.com',
-        port: '',
-        pathname: '/image/upload/**',
-      },
-    ],
   },
 };
 
